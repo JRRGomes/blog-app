d1d3d3d3c097b070eb6b998308dd2cd570ca8f74
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-04-05 11:21:05 -0300

    feat: Created react-app setup to  a blog app. Used groomet as component styled library

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Box, Button, Heading, Grommet } from 'grommet';
+import { Notification } from 'grommet-icons';
+import { theme } from './theme'
+import { AppBar } from './components/AppBar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grommet theme={theme}>
+      <AppBar>
+        My Blog
+        <Heading level='3' margin='none'>Blog Posts</Heading>
+        <Button icon={<Notification />} onClick={()=>{}}></Button>
+      </AppBar>
+      <Box flex align='center' justify='center'>
+       blog body
+     </Box>
+    </Grommet>
   );
 }
 
