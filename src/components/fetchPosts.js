1274a2e8b44f01c4bde0b738d11571420a8ffe4f
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-04-11 16:32:58 -0300

    feat: Created fetchUsers method to fetch the users to the array of posts

--- src/components/fetchPosts.js
+++ src/components/fetchPosts.js
@@ -1,10 +1,36 @@
 import { api } from '../api'
+import axios from 'axios'
+
 
 const pageArray = (total, size) => {
   const totalPages = total / size;
   const fullPages = Math.floor(totalPages);
   const partialPage = total % size;
   return [... new Array(fullPages).fill(size), partialPage].filter(Boolean)
+}
+
+const fetchUser = (userId) => {
+  return axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`)
+      .then((userRes) => {
+        return userRes.data
+      })
+}
+
+const fetchUsers = (posts) => {
+    const uniqueUsers = new Set (posts.map((post) => {
+      return post.userId
+    }))
+    console.log(uniqueUsers)
+    const promises = Array.from(uniqueUsers).map((userId) => {
+      return fetchUser(userId)
+    })
+    return Promise.all(promises)
+    .then((users) => {
+      return posts.map((post) => {
+        const user = users.find(user => user.id === post.userId)
+        return {...post, user}
+      })
+    })
 }
 
 export const fetchPosts = (totalPosts, pageSize) => pageArray(totalPosts, pageSize)
@@ -17,15 +43,25 @@ export const fetchPosts = (totalPosts, pageSize) => pageArray(totalPosts, pageSi
       },
     })
     .then((res) => res.data)
-    .then((res) => 
+    .then((res) =>
       res.map((postObj) => 
         api.get(`/posts/${postObj.id}/comments`)
-        .then((commentsObj) => {
-          postObj.comment = commentsObj.data
-          return postObj
+        .then(({ data: comments }) => {
+          return {...postObj, comments}
         })
       )
     ).then((res) => Promise.all(res))
   })
   .reduce((chain, listPostFn) => chain.then((acc) => listPostFn().then((res) => [...acc, ...res])),
     Promise.resolve([]))
+    .then((posts) => fetchUsers(posts))
+    // .then((posts) => 
+    //   posts.map((postObj) =>
+    //     api.get(`/users/${postObj.userId}`)
+    //     .then(({ data: user }) => {
+    //       return {...postObj, user}
+    //     })
+    //   )
+    // ).then((posts) => Promise.all(posts))
+
+    
